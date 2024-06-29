59d3de80903aa5f5638bb866c71bfa173bfe71dc
housebanao-main <tech@housebanao.com>
2024-06-29 17:05:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import {  HomePage } from './pages/HomePage'
-import { ThankYoupage } from './pages/ThankYoupage'
-
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ThankYoupage } from './pages/ThankYoupage';
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/Home" element={<HomePage />} />
-          <Route path="/Thankyou" element={<ThankYoupage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />  // Redirect from root to /Home
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/ThankYou" element={<ThankYoupage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
